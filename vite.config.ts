cb367602b834fdecf12fb5342e553b6619b0ce97
mkrause <maikel.krause@fortanix.com>
2024-12-09 22:41:29 +0100

    Refactor top-level Sass code.
    - Removes usages of legacy @import statements.
    - Fixed @layer ordering not being emitted at the top of the build file.

--- vite.config.ts
+++ vite.config.ts
@@ -76,7 +76,7 @@ export default defineConfig({
         safari: 17 << 16 | 5 << 8, // Minimum for `light-dark()`
       },
       cssModules: {
-        // @ts-expect-error Will be fixed in vite v6
+        // @ts-expect-error This is fixed in vite v6, remove this line once we upgrade.
         grid: false, // Workaround for https://github.com/parcel-bundler/lightningcss/issues/762
       },
     },
