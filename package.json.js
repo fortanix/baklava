3ebbfe4f42f499a6da97d7bc8bc028e84a254780
mkrause <maikel.krause@fortanix.com>
2024-12-24 14:29:00 +0100

    Add test:storybook-ci command.

--- package.json.js
+++ package.json.js
@@ -79,10 +79,18 @@ const packageConfig = {
     // Test
     // Note: use `vitest run --root=. src/...` to run a single test file
     //'test:unit': 'vitest run --root=.', // Need to specify `--root=.` since the vite root is set to `./app`
-    'test:storybook': 'test-storybook --failOnConsole --browsers chromium', // For text only: FORCE_COLOR=false
-    'test': 'npm run check:types && npm run lint:style', // TODO: add `lint:script`, `test:unit, and `test:storybook`
+    'test': 'npm run check:types && npm run lint:style', // TODO: add `lint:script`, `test:unit`
     'test-ui': 'vitest --ui',
     'coverage': 'vitest run --coverage',
+    
+    // Browser automation tests
+    // https://github.com/storybookjs/test-runner?tab=readme-ov-file#2-running-against-locally-built-storybooks-in-ci
+    'test:storybook': 'test-storybook --failOnConsole --browsers chromium', // For text only: FORCE_COLOR=false
+    'test:storybook-ci': `
+      npx concurrently -k -s first -n "SB,TEST" -c "magenta,blue"\
+        "npm run storybook:build --quiet && npx http-server storybook-static --port 6006 --silent"\
+        "npx wait-on tcp:6006 && npm run test:storybook"
+    `,
     
     // Shorthands
     'start': 'npm run storybook:serve',
