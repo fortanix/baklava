4713564af2ba0720637be0d57385e9446c43ec3f
Arlindo Pereira <arlindosaraivapereira@gmail.com>
2025-01-13 16:25:18 +0100

    PLAT-4148 Select styling fixes

--- src/components/forms/controls/Select/Select.stories.tsx
+++ src/components/forms/controls/Select/Select.stories.tsx
@@ -34,11 +34,11 @@ export const Standard: Story = {
         <Select.Option optionKey="option-1" label="Option 1"/>
         <Select.Option optionKey="option-2" label="Option 2"/>
         <Select.Option optionKey="option-3" label="Option 3"/>
-        <Select.Option optionKey="option-4" label="Option 4"/>
+        {/* <Select.Option optionKey="option-4" label="Option 4"/>
         <Select.Option optionKey="option-5" label="Option 5"/>
         <Select.Option optionKey="option-6" label="Option 6"/>
         <Select.Option optionKey="option-7" label="Option 7"/>
-        <Select.Option optionKey="option-8" label="Option 8"/>
+        <Select.Option optionKey="option-8" label="Option 8"/> */}
       </>
     ),
   },
