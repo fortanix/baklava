3488d85238870e97bdcfc13f2eb9300ac4b58565
Imad A. Bakir <imad.bakir@fortanix.com>
2025-01-14 07:47:01 +0100

    Data Table: Fix formatting issue

--- src/components/tables/DataTable/pagination/Pagination.tsx
+++ src/components/tables/DataTable/pagination/Pagination.tsx
@@ -8,81 +8,81 @@ import { Icon } from "../../../graphics/Icon/Icon.tsx";
 import { Input } from "../../../forms/controls/Input/Input.tsx";
 
 import {
-	type PageSizeOption,
-	PaginationSizeSelector,
+  type PageSizeOption,
+  PaginationSizeSelector,
 } from "./PaginationSizeSelector.tsx";
 import { useTable } from "../DataTableContext.tsx";
 
 import "./Pagination.scss";
 
 type PaginationProps = {
-	pageSizeOptions?: Array<PageSizeOption>;
+  pageSizeOptions?: Array<PageSizeOption>;
 };
 export const Pagination = ({ pageSizeOptions }: PaginationProps) => {
-	const { table } = useTable();
+  const { table } = useTable();
 
-	/*
-	Available pagination state:
-	- table.state.pageIndex
-	- table.state.pageSize
-	- table.canPreviousPage
-	- table.canNextPage
-	- table.pageOptions
-	- table.pageCount
-	- table.gotoPage
-	- table.nextPage
-	- table.previousPage
-	- table.setPageSize
-	*/
+  /*
+  Available pagination state:
+  - table.state.pageIndex
+  - table.state.pageSize
+  - table.canPreviousPage
+  - table.canNextPage
+  - table.pageOptions
+  - table.pageCount
+  - table.gotoPage
+  - table.nextPage
+  - table.previousPage
+  - table.setPageSize
+  */
 
-	return (
-		<div className="pagination">
-			<PaginationSizeSelector pageSizeOptions={pageSizeOptions} />
+  return (
+    <div className="pagination">
+      <PaginationSizeSelector pageSizeOptions={pageSizeOptions} />
 
-			<div className="pager pager--indexed">
-				<Icon
-					icon="page-backward"
-					className={cx("pager__nav pager__nav--prev", {
-						disabled: !table.canPreviousPage,
-					})}
-					onClick={() => table.gotoPage(0)}
-				/>
-				<div className="pagination-main">
-					<Icon
-						icon="caret-left"
-						className={cx("pager__nav pager__nav--prev", {
-							disabled: !table.canPreviousPage,
-						})}
-						onClick={table.previousPage}
-					/>
-					<Input
-						type="number"
-						className="pagination__page-input"
-						value={table.state.pageIndex + 1}
-						max={table.pageCount}
-						min={1}
-						onChange={(event) =>
-							table.gotoPage(Number.parseInt(event.target.value) - 1)
-						}
-					/>
-					of {table.pageCount}
-					<Icon
-						icon="caret-right"
-						className={cx("pager__nav pager__nav--next", {
-							disabled: !table.canNextPage,
-						})}
-						onClick={table.nextPage}
-					/>
-				</div>
-				<Icon
-					name="chevron-right"
-					icon="page-forward"
-					className={cx("pager__nav pager__nav--next", {
-						disabled: !table.canNextPage,
-					})}
-					onClick={() => table.gotoPage(table.pageCount - 1)}
-				/>
-			</div>
-		</div>
-	);
+      <div className="pager pager--indexed">
+        <Icon
+          icon="page-backward"
+          className={cx("pager__nav pager__nav--prev", {
+            disabled: !table.canPreviousPage,
+          })}
+          onClick={() => table.gotoPage(0)}
+        />
+        <div className="pagination-main">
+          <Icon
+            icon="caret-left"
+            className={cx("pager__nav pager__nav--prev", {
+              disabled: !table.canPreviousPage,
+            })}
+            onClick={table.previousPage}
+          />
+          <Input
+            type="number"
+            className="pagination__page-input"
+            value={table.state.pageIndex + 1}
+            max={table.pageCount}
+            min={1}
+            onChange={(event) =>
+              table.gotoPage(Number.parseInt(event.target.value) - 1)
+            }
+          />
+          of {table.pageCount}
+          <Icon
+            icon="caret-right"
+            className={cx("pager__nav pager__nav--next", {
+              disabled: !table.canNextPage,
+            })}
+            onClick={table.nextPage}
+          />
+        </div>
+        <Icon
+          name="chevron-right"
+          icon="page-forward"
+          className={cx("pager__nav pager__nav--next", {
+            disabled: !table.canNextPage,
+          })}
+          onClick={() => table.gotoPage(table.pageCount - 1)}
+        />
+      </div>
+    </div>
+  );
 };
