ec1002b431d615361437989159b7b9d7964e1f37
mkrause <maikelkrause@gmail.com>
2025-02-10 10:38:20 +0100

    Experiment: hide visibility icon if there is no input.

--- src/components/forms/controls/Input/InputPassword.tsx
+++ src/components/forms/controls/Input/InputPassword.tsx
@@ -6,22 +6,48 @@ import * as React from 'react';
 import { Input } from './Input.tsx';
 
 
+type InputPasswordRef = undefined | (React.ComponentRef<typeof Input> & {
+  bkToggleVisibility: () => void,
+});
+type InputPasswordProps = Omit<React.ComponentProps<typeof Input>, 'ref'> & {
+  ref?: undefined | React.Ref<InputPasswordRef>,
+};
 /** A password input control. */
-export const InputPassword = (props: React.ComponentProps<typeof Input>) => {
+export const InputPassword = ({ ref, ...propsRest }: InputPasswordProps) => {
+  const inputRef = React.useRef<React.ComponentRef<typeof Input>>(null);
+  
   const [isVisible, setIsVisible] = React.useState(false);
+  const toggleVisibility = React.useCallback(() => { setIsVisible(visible => !visible); }, []);
+  
+  React.useImperativeHandle(
+    ref,
+    () => inputRef.current ? ({ ...inputRef.current, bkToggleVisibility: toggleVisibility }) : undefined,
+    [toggleVisibility],
+  );
+  
+  // Note: this would be easier if this were always a controlled component, but this component is designed to be
+  // used as either controlled or uncontrolled.
+  const [showIcon, setShowIcon] = React.useState(false);
+  const handleChange = React.useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    const isEmpty = event.target.value === '';
+    setShowIcon(!isEmpty);
+  }, []);
   
   return (
     <Input
+      ref={inputRef}
       type={isVisible ? 'text' : 'password'}
       placeholder="Password"
       actions={
         <Input.Action
+          hidden={!showIcon}
           icon={isVisible ? 'eye-open' : 'eye-closed'}
           label={isVisible ? 'Password is visible' : 'Password is hidden'}
-          onPress={() => { setIsVisible(visible => !visible); }}
+          onPress={toggleVisibility}
         />
       }
-      {...props}
+      onChange={handleChange}
+      {...propsRest}
     />
   );
 };
