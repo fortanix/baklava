69d1ce3eb07fc1ead07e0c5617236b6a40ab4b95
Mahek-Vasoya <mahek.vasoya@fortanix.com>
2025-05-09 22:31:42 +0530

    import file extension added

--- src/fortanix/Logo/Logo.tsx
+++ src/fortanix/Logo/Logo.tsx
@@ -2,7 +2,7 @@
 |* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
 |* the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { classNames as cx, type ComponentProps } from '../../util/componentUtil';
+import { classNames as cx, type ComponentProps } from '../../util/componentUtil.ts';
 import * as React from 'react';
 
 import logoImage from  '../../assets/fortanix/fortanix_logo_icon.svg';
