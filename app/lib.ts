f5d85f605eccbf07b1ca389fd8ab0c9039700403
mkrause <maikelkrause@gmail.com>
2025-05-17 23:40:12 +0200

    Rename Logo to FortanixLogo.

--- app/lib.ts
+++ app/lib.ts
@@ -112,7 +112,13 @@ export { AccountSelector } from '../src/layouts/AppLayout/Header/AccountSelector
 export { SolutionSelector } from '../src/layouts/AppLayout/Header/SolutionSelector.tsx';
 export { SysadminSwitcher } from '../src/layouts/AppLayout/Header/SysadminSwitcher.tsx';
 export { UserMenu } from '../src/layouts/AppLayout/Header/UserMenu.tsx';
-export { FortanixLogo as Logo } from '../src/fortanix/FortanixLogo/FortanixLogo.tsx';
 export { Nav } from '../src/layouts/AppLayout/Nav/Nav.tsx';
 export { Sidebar } from '../src/layouts/AppLayout/Sidebar/Sidebar.tsx';
 export { AppLayout } from '../src/layouts/AppLayout/AppLayout.tsx';
+
+/**
+ * Fortanix-specific.
+ */
+
+export { FortanixLogo } from '../src/fortanix/FortanixLogo/FortanixLogo.tsx';
+export { FortanixLogo as Logo } from '../src/fortanix/FortanixLogo/FortanixLogo.tsx'; // Temporarily alias as `Logo` until migrations are complete
