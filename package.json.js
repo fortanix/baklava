b12bb2d96a7814596dcb616ee3aa77de1792d4d7
mkrause <maikelkrause@gmail.com>
2025-05-21 11:04:32 +0200

    Release v1.0.0-beta-20250521-2

--- package.json.js
+++ package.json.js
@@ -8,7 +8,7 @@ import * as fs from 'node:fs';
 
 const packageConfig = {
   name: '@fortanix/baklava',
-  version: '1.0.0-beta-20250521',
+  version: '1.0.0-beta-20250521-2',
   license: 'MPL-2.0',
   author: 'Fortanix',
   description: 'Fortanix Baklava design system',
