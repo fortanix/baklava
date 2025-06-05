c9bd83e3fd1e070100438496efaf65a42b834c98
mkrause <maikelkrause@gmail.com>
2025-06-05 18:22:53 +0200

    Render DialogModal inside of a portal.

--- src/components/util/overlays/modal/ModalProvider.tsx
+++ src/components/util/overlays/modal/ModalProvider.tsx
@@ -3,6 +3,7 @@
 |* the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import * as React from 'react';
+import { createPortal } from 'react-dom';
 
 import { useDelayedUnmount } from '../../../../util/hooks/useDelayedUnmount.ts';
 import { useModalDialogTracker } from '../TopLayerManager.tsx';
@@ -84,7 +85,7 @@ export const ModalProvider = Object.assign(
     
     return (
       <>
-        {shouldMount && dialog(dialogProps)}
+        {shouldMount && createPortal(dialog(dialogProps), document.body)}
         {typeof children === 'function' ? children(modalRef) : children}
       </>
     );
