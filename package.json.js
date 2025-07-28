04f53e3d5486fc717a60aee444ee88020c064ae6
Mahek Vasoya <mahek.vasoya@fortanix.com>
2025-07-28 13:32:10 +0530

    Release v1.0.0-beta-20250728 (#441)

--- package.json.js
+++ package.json.js
@@ -8,7 +8,7 @@ import * as fs from 'node:fs';
 
 const packageConfig = {
   name: '@fortanix/baklava',
-  version: '1.0.0-beta-20250725',
+  version: '1.0.0-beta-20250728',
   license: 'MPL-2.0',
   author: 'Fortanix',
   description: 'Fortanix Baklava design system',
