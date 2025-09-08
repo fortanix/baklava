f680c41f4b3748ec0218f20c351bccee226f2e3f
Arlindo Pereira <arlindosaraivapereira@gmail.com>
2025-09-08 10:38:14 +0200

    run code formatter on .storybook/main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,8 +2,8 @@
 |* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
 |* the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { type StorybookConfig } from '@storybook/react-vite'
-import { withoutVitePlugins } from '@storybook/builder-vite'
+import { type StorybookConfig } from '@storybook/react-vite';
+import { withoutVitePlugins } from '@storybook/builder-vite';
 
 const config: StorybookConfig = {
   core: {
@@ -17,13 +17,24 @@ const config: StorybookConfig = {
     },
   },
 
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
 
-  addons: [// Accessibility
-  '@storybook/addon-a11y', // https://github.com/chromaui/storybook-addon-pseudo-states/issues/101
-  // Allows creating links from one story to another
-  '@storybook/addon-links', //'storybook-addon-pseudo-states',
-  '@storybook/addon-designs', '@chromatic-com/storybook', '@storybook-community/storybook-dark-mode', '@storybook/addon-docs', '@storybook/addon-vitest'],
+  addons: [
+    '@storybook/addon-a11y', // Accessibility
+    '@storybook/addon-links', // Allows creating links from one story to another
+    '@storybook/addon-designs',
+    '@chromatic-com/storybook',
+    '@storybook-community/storybook-dark-mode',
+    '@storybook/addon-docs',
+    '@storybook/addon-vitest',
+
+    // Note: `pseudo-states` does not work properly due to bugs like these:
+    // https://github.com/chromaui/storybook-addon-pseudo-states/issues/101
+    //'storybook-addon-pseudo-states',
+  ],
   typescript: {
     reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
@@ -39,8 +50,8 @@ const config: StorybookConfig = {
   async viteFinal(config) {
     // Remove `vite-dts` plugin since it can cause some issues on storybook builds (and isn't necessary)
     // https://github.com/qmhc/vite-plugin-dts/issues/275
-    config.plugins = await withoutVitePlugins(config.plugins, ['vite:dts'])
-    return config
+    config.plugins = await withoutVitePlugins(config.plugins, ['vite:dts']);
+    return config;
   },
-}
-export default config
+};
+export default config;
