18920453b7376fb30bef23edf8a8caa7aca531da
Mahek-Vasoya <mahek.vasoya@fortanix.com>
2025-12-16 19:51:35 +0530

    Updated fix

--- src/components/tables/DataTable/plugins/useRowSelectColumn.tsx
+++ src/components/tables/DataTable/plugins/useRowSelectColumn.tsx
@@ -32,11 +32,9 @@ export const useRowSelectColumn = <D extends object>(hooks: ReactTable.Hooks<D>)
                   <Checkbox
                     aria-label="Select all rows"
                     checked={checked}
-                    onClick={(e) => e.stopPropagation()}
-                    onChange={(e) => {
-                      e.stopPropagation();
-                      onChange?.(e);
-                    }}
+                    // Prevent the click event from triggering a click on the parent table header/cell
+                    onClick={event => { event.stopPropagation(); }}
+                    onChange={onChange}
                     className={cl['bk-data-table-row-select__checkbox']}
                   />
                 )}
@@ -56,11 +54,9 @@ export const useRowSelectColumn = <D extends object>(hooks: ReactTable.Hooks<D>)
                 <Checkbox
                   aria-label="Select row"
                   checked={checked}
-                  onClick={(e) => e.stopPropagation()}
-                  onChange={(e) => {
-                    e.stopPropagation();
-                    onChange?.(e);
-                  }}
+                  // Prevent the click event from triggering a click on the parent table header/cell
+                  onClick={event => { event.stopPropagation(); }}
+                  onChange={onChange}
                   className={cl['bk-data-table-row-select__checkbox']}
                 />
                 {cellContent}
