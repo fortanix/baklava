5df8118f145df1e778ce9a2e986a2ecd7d171a6f
mkrause <maikelkrause@gmail.com>
2026-01-07 14:45:22 +0100

    Tabs: support passing additional props to each tab button through tabTriggerProps.

--- src/components/navigations/Tabs/Tabs.tsx
+++ src/components/navigations/Tabs/Tabs.tsx
@@ -13,20 +13,29 @@ export { cl as TabsClassNames };
 export type TabKey = string;
 
 export type TabProps = ComponentProps<'div'> & {
-  children?: undefined | React.ReactNode,
+  /** A unique identifier for this tab. */
   tabKey: string,
+  /** The contents of the tab, when active. */
+  children?: undefined | React.ReactNode,
+  /** A render prop to render the tab. Overrides `children`, if set. */
+  render?: undefined | (() => React.ReactNode),
+  /** The title of this tab, to be shown in the tab trigger button. */
   title: React.ReactNode,
-  hide?: boolean,
-  contentClassName?: ClassNameArgument,
-  render?: () => React.ReactNode,
+  /** Whether to hide this tab. If true, the tab trigger is not rendered at all. */
+  hide?: undefined | boolean,
+  /** A class name to set on the tab trigger button. */
+  className?: undefined | ClassNameArgument,
+  /** A class name to set on the tab panel content. */
+  contentClassName?: undefined | ClassNameArgument,
+  /** Any additional props to pass to the tab trigger button. */
+  tabTriggerProps?: undefined | (ComponentProps<'li'> & { 'data-label': string }),
 };
 export const Tab = ({ children }: TabProps): React.ReactElement => {
   return children as React.ReactElement;
 };
 type TabElement = React.ReactElement<TabProps, React.FunctionComponent<typeof Tab>>;
 
-
-export type TabsProps = React.PropsWithChildren<ComponentProps<'div'> & {
+export type TabsProps = ComponentProps<'div'> & {
   /** Whether this component should be unstyled. */
   unstyled?: undefined | boolean,
   
@@ -35,7 +44,7 @@ export type TabsProps = React.PropsWithChildren<ComponentProps<'div'> & {
   
   /** Callback executed when active tab is changed. */
   onSwitch: (tabKey: TabKey) => void,
-}>;
+};
 /**
  * A tab component
  */
@@ -54,7 +63,7 @@ export const Tabs = (props: TabsProps) => {
   
   type ActiveTabProps = Omit<TabProps, 'children' | 'tabKey' | 'title' | 'hide' | 'render'>;
   const getActiveTabProps = (tab: TabElement): ActiveTabProps => {
-    const { children, tabKey, title, hide, render, ...tabProps } = tab.props;
+    const { children, tabKey, title, hide, render, tabTriggerProps, ...tabProps } = tab.props;
     return tabProps;
   };
   
@@ -91,23 +100,28 @@ export const Tabs = (props: TabsProps) => {
     >
       <ul className={cx(cl['bk-tabs__switcher'])} role="tablist">
         {tabs.map(tab => {
-          if (tab.props.hide) return null;
-          const isActive = tab.props.tabKey === activeKey;
+          const { tabKey, hide, tabTriggerProps } = tab.props;
+          
+          if (hide) { return null; }
+          
+          const isActive = tabKey === activeKey;
+          
           return (
             <li
-              key={tab.props.tabKey}
+              key={tabKey}
               role="tab"
               tabIndex={0}
               aria-selected={isActive}
-              data-tab={tab.props.tabKey}
-              className={cx(cl['bk-tabs__switcher__tab'],tab.props.className)}
+              data-tab={tabKey}
+              {...tabTriggerProps}
+              className={cx(cl['bk-tabs__switcher__tab'], tabTriggerProps?.className)}
               onClick={() => { onSwitch(tab.props.tabKey); }} // FIXME: add a Button and use that instead
             >
               <span>{tab.props.title}</span>
               {/* Hidden duplicated title, used to prevent layout shifts on hover. */}
               <span aria-hidden className={cx(cl['bk-tabs__switcher__tab__hover-placeholder'])}>{tab.props.title}</span>
             </li>
-          )
+          );
         })}
       </ul>
       
