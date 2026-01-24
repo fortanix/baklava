678581c8fcdf686d872762c039e8a062e965e77d
mkrause <maikelkrause@gmail.com>
2026-01-25 00:12:28 +0700

    CompoundList: Work on type-ahead search.

--- src/util/hooks/useTypeAhead.ts
+++ src/util/hooks/useTypeAhead.ts
@@ -2,8 +2,15 @@
 |* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
 |* the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { removeCombiningCharacters } from '../formatting.ts';
 import * as React from 'react';
 
+
+export const normalizeQueryString = (query: string) => {
+  // For type-ahead search, we will want to ignore whitespace, such that the user does not need to match the
+  // whitespace exactly
+  return removeCombiningCharacters(query.trim().toLocaleLowerCase()).replaceAll(/\s+/g, '');
+};
 
 /**
  * Track the user's typing to drive a type-ahead search.
@@ -18,12 +25,16 @@ export const useTypeAhead = (maxDuration = 400/*ms*/) => {
     const shouldReset = now - lastKeyPressTime.current > maxDuration;
     lastKeyPressTime.current = now;
     
-    setSequence((prevSequence) => {
+    setSequence(prevSequence => {
       const currentSequence = shouldReset ? [] : prevSequence;
       
       // Ignore isolated space inputs, since we likely instead want this to trigger an action (e.g. clicking a button
       // or selecting a form input).
       if (event.key === ' ') { return currentSequence; }
+      
+      if (event.key === 'Backspace') {
+        return currentSequence.slice(0, -1);
+      }
       
       const isPrintable = event.key.length === 1; // For control characters, `key` will be a longer word (e.g. `Tab`)
       
@@ -36,6 +47,8 @@ export const useTypeAhead = (maxDuration = 400/*ms*/) => {
       
       if (!isPrintable || hasModifier || isInput) { return currentSequence; }
       
+      // Note: only do the following for actual search input characters, we don't want to `preventDefault()` for
+      // control key inputs like 'Enter' or 'Space' for example.
       event.preventDefault();
       event.stopPropagation();
       
@@ -43,5 +56,6 @@ export const useTypeAhead = (maxDuration = 400/*ms*/) => {
     });
   }, [maxDuration]);
   
-  return { handleKeyDown, sequence };
+  const query = normalizeQueryString(sequence.join(''));
+  return { handleKeyDown, sequence, query };
 };
