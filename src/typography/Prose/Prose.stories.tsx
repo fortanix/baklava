51b93e30512ae92dcebdf5376429fabae98e3944
mkrause <maikelkrause@gmail.com>
2026-04-06 23:50:10 +0200

    Prose: refactoring.

--- src/typography/Prose/Prose.stories.tsx
+++ src/typography/Prose/Prose.stories.tsx
@@ -59,16 +59,27 @@ const SampleProse = ({ heading }: { heading?: undefined | React.ReactNode }) => 
         {' '}<i>This text is italic.</i>
         {' '}<u>This text is underlined.</u>
       </p>
-      
-      <blockquote>This is a block quote.</blockquote>
+      <p>
+        Here is some inline code: <code>{'<Prose>code</Prose>'}</code>.
+      </p>
+      <pre>{`
+        This is a <pre> block.
+        Here is another line.
+      `.trim()}</pre>
       
       <hr/>
       
       <p>
         Lorem ipsum dolor sit amet, <DummyLink>consectetur</DummyLink> adipiscing elit. Pellentesque eget sem ut neque lobortis pharetra nec vel quam. Etiam sem neque, gravida sed pharetra ut, vehicula quis lectus. Donec ac rhoncus purus. Proin ultricies augue vitae purus feugiat, in ultrices lorem aliquet. Donec eleifend ac dolor a auctor. Cras ac suscipit nibh. Fusce tincidunt iaculis dapibus. Vivamus sit amet neque eu velit tincidunt semper. Donec at magna aliquam mi consectetur imperdiet. Donec pretium placerat quam, in sodales purus porta vitae. Phasellus nisl justo, luctus vel mi vel, sollicitudin euismod neque.
       </p>
+      
+      <blockquote>This is a block quote.</blockquote>
+      
       <p>
-        Duis mollis, justo vel pretium luctus, risus sem eleifend lectus, ac convallis dolor nibh id sapien. Donec vestibulum tellus non rutrum convallis. Aenean venenatis enim in egestas lobortis. Donec mollis elit in turpis imperdiet congue vel at magna. Vestibulum bibendum, ipsum quis lobortis lobortis, lorem libero mollis sapien, sed tempus lacus nibh a nunc. Vivamus sed sem eleifend, rutrum erat eget, ultricies neque. Morbi condimentum dolor vel ipsum consectetur iaculis. Donec ornare diam at orci luctus, sed placerat quam dictum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur condimentum molestie augue. Ut vel nisl a augue ornare volutpat. Phasellus et enim in mi maximus ultricies. Integer dignissim ipsum mauris, id bibendum eros euismod et.
+        Duis mollis, justo vel pretium luctus, risus sem eleifend lectus, ac convallis dolor nibh id sapien. Donec vestibulum tellus non rutrum convallis. Aenean venenatis enim in egestas lobortis. Donec mollis elit in turpis imperdiet congue vel at magna. Vestibulum bibendum, ipsum quis lobortis lobortis, lorem libero mollis sapien, sed tempus lacus nibh a nunc. Vivamus sed sem eleifend, rutrum erat eget, ultricies neque.
+      </p>
+      <p>
+        Morbi condimentum dolor vel ipsum consectetur iaculis. Donec ornare diam at orci luctus, sed placerat quam dictum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur condimentum molestie augue. Ut vel nisl a augue ornare volutpat. Phasellus et enim in mi maximus ultricies. Integer dignissim ipsum mauris, id bibendum eros euismod et.
       </p>
       <p>An unordered list:</p>
       <ul>
@@ -219,14 +230,14 @@ export const ProseImmediate: Story = {
 /** Elements with class `.bk` or `.bk-isolate` should be isolated from prose styling. */
 export const ProseIsolation: Story = {
   render: () => (
-    <h1 className="bk-prose">
-      I should be styled as an h1
+    <strong className="bk-prose">
+      I should be bold.<br/>
       
-      <h2>I should be styled as an h2</h2>
+      <em>I should be bold + italic</em><br/>
       
-      <h2 className="bk">I should not be styled</h2>
-      <h2 className="bk-isolate">I should also not be styled</h2>
-    </h1>
+      <em className="bk">I have <code>className="bk"</code>, and should have default styling</em><br/>
+      <em className="bk-isolate">I have <code>className="bk-isolate"</code>, and should have default styling</em><br/>
+    </strong>
   ),
 };
 
