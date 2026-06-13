4b754e2e0f9378fb84119afd886d1f2a5673aadb
mkrause <maikelkrause@gmail.com>
2026-06-13 23:26:43 +0200

    Implement new Collection utility and use it to rework SegmentedControl.

---
+++ src/components/util/Collection/Collection.stories.tsx
@@ -0,0 +1,288 @@
+
+import * as React from 'react';
+import { mergeProps } from '../../../util/reactUtil.ts';
+
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import { FocusGroup } from '../FocusGroup/FocusGroup.tsx';
+import { Button } from '../../actions/Button/Button.tsx';
+
+import { type ItemKey, useCollectionItem, useCollection } from './Collection.tsx';
+
+
+// An example `Collection` component
+type CollectionItemProps = React.ComponentProps<typeof Button> & { itemKey: ItemKey };
+const CollectionItem = ({ itemKey, ...propsRest }: CollectionItemProps) => {
+  const itemProps = useCollectionItem({ itemKey });
+  return (
+    <Button {...mergeProps(propsRest, itemProps, { className: 'story-collection-item' })} icon="file"/>
+  );
+};
+const Collection = Object.assign(
+  (props: React.ComponentProps<typeof FocusGroup>) => {
+    const coll = useCollection<React.ComponentRef<typeof FocusGroup>>();
+    return (
+      <coll.Provider>
+        <FocusGroup {...mergeProps(props, coll.props)} role="listbox" focusGroup="listbox">
+          <style>{`
+            @scope {
+              display: flex;
+              flex-direction: column;
+              
+              .story-collection-item {
+                content-visibility: auto;
+                contain-intrinsic-inline-size: none;
+                contain-intrinsic-block-size: auto 2lh;
+              }
+              /*
+              Test 'content-visibility' behavior. Note: apply the height to the inner icon, since browsers are smart
+              enough to see the 'height' when applied to the item itself.
+              */
+              .story-collection-item:nth-child(n + 80) > * {
+                height: 10lh;
+              }
+            }
+          `}</style>
+          {props.children}
+        </FocusGroup>
+      </coll.Provider>
+    );
+  },
+  { Item: CollectionItem },
+);
+
+const ScrollContainer = ({ children }: React.PropsWithChildren) => {
+  return (
+    <div>
+      <style>{`
+        @scope {
+          outline: 1px solid purple;
+          
+          overflow-inline: hidden;
+          overflow-block: auto;
+          
+          inline-size: 20ch;
+          max-block-size: 20lh;
+          padding: 0.8lh 1.4em;
+        }
+      `}</style>
+      {children}
+    </div>
+  );
+};
+
+
+type CollectionArgs = React.ComponentProps<typeof Collection>;
+type Story = StoryObj<CollectionArgs>;
+
+export default {
+  component: Collection,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+  args: {},
+  // Note: this doesn't work well since local decorators are applied in an unintuitive order:
+  // https://github.com/storybookjs/storybook/issues/23151
+  //decorators: [Story => <ScrollContainer><Story/></ScrollContainer>],
+  render: (args) => <ScrollContainer><Collection {...args}/></ScrollContainer>,
+} satisfies Meta<CollectionArgs>;
+
+
+//
+// Sample items
+//
+
+const fruits = [
+  'Apple',
+  'Apricot',
+  'Blueberry',
+  'Cherry',
+  'Durian',
+  'Jackfruit',
+  'Melon',
+  'Mango',
+  'Mangosteen',
+  'Orange',
+  'Peach',
+  'Pineapple',
+  'Razzberry',
+  'Strawberry',
+];
+
+const generateItems = (itemCount: number) =>
+  Object.fromEntries(Array.from({ length: itemCount }, (_, i) => i + 1).map(index => [`item-${index}`, {
+    itemKey: `item-${index}`,
+    label: `Item ${index}`,
+  }]));
+
+
+
+export const CollectionStandard: Story = {
+  args: {
+    children: (
+      <>
+        <CollectionItem itemKey="item-1" label="Item 1"/>
+        <CollectionItem itemKey="item-2" label="Item 2"/>
+        <CollectionItem itemKey="item-3" label="Item 3"/>
+      </>
+    ),
+  },
+};
+
+
+
+type ColumnProps = React.ComponentProps<typeof Collection> & { itemKey: ItemKey };
+const Column = ({ itemKey, ...propsRest }: ColumnProps) => {
+  const itemProps = useCollectionItem({ itemKey });
+  return (
+    <Collection {...mergeProps(propsRest, itemProps)}/>
+  );
+};
+type CollectionTwoColumnsProps = {
+  left: React.ReactNode,
+  right: React.ReactNode,
+};
+const CollectionTwoColumns = ({ left, right }: CollectionTwoColumnsProps) => {
+  const { Provider: CollectionProvider, props } = useCollection<HTMLDivElement>();
+  
+  React.useEffect(() => {
+    //...
+  }, []);
+  
+  return (
+    <ScrollContainer>
+      <CollectionProvider>
+        <div {...props}>
+          <style>{`
+            @scope {
+              display: flex;
+              flex-direction: column;
+            }
+          `}</style>
+          
+          <Column itemKey="left">{left}</Column>
+          <Column itemKey="right">{right}</Column>
+        </div>
+      </CollectionProvider>
+    </ScrollContainer>
+  );
+};
+
+export const CollectionWithColumns: Story = {
+  render: () => (
+    <CollectionTwoColumns
+      left={
+        <>
+          <CollectionItem itemKey="left-1">Left 1</CollectionItem>
+          <CollectionItem itemKey="left-2">Left 2</CollectionItem>
+        </>
+      }
+      right={
+        <>
+          <CollectionItem itemKey="right-1">right 1</CollectionItem>
+          <CollectionItem itemKey="right-2">right 2</CollectionItem>
+        </>
+      }
+    />
+  ),
+};
+
+
+
+const CollectionWithControlsC = (args: CollectionArgs) => {
+  // const [isTransitionPending, startTransition] = React.useTransition();
+  const startTransition = (fn: any) => fn();
+  
+  const [itemCount, setItemCount] = React.useState(100);
+  const [items, setItems] = React.useState<Record<ItemKey, CollectionItemProps>>(() => generateItems(itemCount));
+  
+  React.useEffect(() => {
+    setItems(generateItems(itemCount));
+  }, [itemCount]);
+  
+  const randomizeItems = React.useCallback(() => {
+    setItems(items => {
+      return Object.fromEntries(Object.entries(items)
+        .toSorted(() => Math.random() >= 0.5 ? 1 : -1)
+      );
+    });
+  }, []);
+  
+  // Randomize the items and inject one new item in the middle
+  const randomizeItemsAndSplice = React.useCallback(() => {
+    setItems(items => {
+      const entries = Object.entries(items)
+        .sort(() => Math.random() >= 0.5 ? 1 : -1);
+      
+      const randomId = (Math.round(Date.now())).toString(36);
+      const newItem = fruits.at(Math.floor(Math.random() * fruits.length));
+      if (!newItem) { throw new Error(`Should not happen`); }
+      const newItemKey = `${newItem}-${randomId}`;
+      
+      const startIndex = Math.floor(entries.length / 2);
+      entries.splice(startIndex, 1, [newItemKey, { itemKey: newItemKey, children: newItemKey }]);
+      
+      return Object.fromEntries(entries);
+    });
+  }, []);
+  
+  const appendItem = React.useCallback(() => {
+    const newItem = fruits.at(Math.floor(Math.random() * fruits.length));
+    if (!newItem) { throw new Error(`Should not happen`); }
+    
+    setItems(items => {
+      const newItemKey = `${newItem}-${Object.keys(items).length}`;
+      return { ...items, [newItemKey]: { itemKey: newItemKey, children: newItemKey } };
+    });
+  }, []);
+  
+  const prependItem = React.useCallback(() => {
+    const newItem = fruits.at(Math.floor(Math.random() * fruits.length));
+    if (!newItem) { throw new Error(`Should not happen`); }
+    
+    setItems(items => {
+      const newItemKey = `${newItem}-${Object.keys(items).length}`;
+      return { [newItemKey]: { itemKey: newItemKey, children: newItemKey }, ...items };
+    });
+  }, []);
+  
+  return (
+    <div>
+      <style>{`
+        @scope {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          
+          .story-collection {}
+        }
+      `}</style>
+      
+      <div>
+        <style>{`@scope { margin-block: 0.4lh; display: flex; gap: 1ch; align-items: center; }`}</style>
+        <Button kind="secondary" label="10" onPress={() => { startTransition(() => { setItemCount(10); }); }}/>
+        <Button kind="secondary" label="100" onPress={() => { startTransition(() => { setItemCount(100); }); }}/>
+        <Button kind="secondary" label="1K" onPress={() => { startTransition(() => { setItemCount(1000); }); }}/>
+        <Button kind="secondary" label="10K" onPress={() => { startTransition(() => { setItemCount(10_000); }); }}/>
+      </div>
+      
+      <ScrollContainer>
+        <Collection {...args} className="story-collection">
+          {Object.entries(items).map(([itemKey, itemProps]) =>
+            <Collection.Item key={itemKey} {...itemProps}/>
+          )}
+        </Collection>
+      </ScrollContainer>
+      
+      <Button label="Append" onPress={() => { startTransition(() => { appendItem(); }); }}/>
+      <Button label="Prepend" onPress={() => { startTransition(() => { prependItem(); }); }}/>
+      <Button label="Randomize" onPress={() => { startTransition(() => { randomizeItems(); }); }}/>
+      <Button label="Randomize + splice" onPress={() => { startTransition(() => { randomizeItemsAndSplice(); }); }}/>
+    </div>
+  );
+};
+export const CollectionWithControls: Story = {
+  decorators: (_, { args }) => <CollectionWithControlsC {...args}/>,
+};
---
+++ src/components/util/Collection/Collection.tsx
@@ -0,0 +1,248 @@
+/* Copyright (c) Fortanix, Inc.
+|* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
+|* the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import * as React from 'react';
+import { useMemoOnce } from '../../../util/reactUtil.ts';
+
+
+export type ItemKey = string;
+
+const internalKey = Symbol('baklava.Collection.internal');
+export type CollectionContext = {
+  /**
+   * Internal bookkeeping. Updated through mutation to support a large number of items and prevent the cost of
+   * immutable updates to the registry. Note: this `registry` is unordered.
+   */
+  [internalKey]: {
+    registry: Map<ItemKey, Element>,
+    dirty: boolean,
+  },
+  
+  /** Unique component ID for the current collection component. */
+  componentId: string,
+};
+export const CollectionContext = React.createContext<null | CollectionContext>(null);
+
+export const useCollection = /*<E extends Element>*/() => {
+  const componentId = React.useId();
+  //const ref = React.useRef<E>(null);
+  
+  // Note: we want collections to scale to a large amount of items. That means the following state should in principle
+  // never be updated. `componentId` is an exception, which should normally not be updated by the consumer. If you
+  // need state that can be updated, use something like zustand instead of React context.
+  const contextInternal = useMemoOnce<CollectionContext[typeof internalKey]>(() => ({
+    registry: new Map(),
+    dirty: true,
+  }));
+  const context = React.useMemo(() => ({
+    [internalKey]: contextInternal,
+    componentId,
+  }), [componentId]);
+  
+  const Provider = useMemoOnce(() => ({ children }: React.PropsWithChildren) =>
+    <CollectionContext value={context}>{children}</CollectionContext>,
+  );
+  
+  // TODO: move this inside the `Provider` instead?
+  React.useLayoutEffect(() => {
+    if (contextInternal.dirty) {
+      console.log('CHANGED', contextInternal.registry.keys().toArray()); // TEMP
+      contextInternal.dirty = false;
+    }
+  });
+  
+  return {
+    Provider,
+    props: {
+      //ref,
+      'data-bk-coll-id': context.componentId,
+    },
+  };
+};
+
+type UseCollectionItemParams = { itemKey: ItemKey };
+type UseCollectionItemResult<E extends Element> = {
+  ref: React.RefCallback<E>,
+  'data-bk-coll-parent': string,
+  'data-bk-coll-item': string,
+};
+export const useCollectionItem = <E extends Element>(params: UseCollectionItemParams): UseCollectionItemResult<E> => {
+  const { itemKey } = params;
+  
+  const context = React.use(CollectionContext);
+  if (context === null) { throw new Error(`Missing CollectionContext provider`); }
+  const contextInternal = context[internalKey];
+  
+  // TEMP: debug
+  //Object.assign(window, { _c: context, _ci: context[internalKey] });
+  
+  React.useLayoutEffect(() => {
+    //console.log('child re-render', itemKey);
+  });
+  
+  const ref = React.useCallback<React.RefCallback<E>>(el => {
+    if (typeof itemKey === 'undefined') {
+      console.warn(`[Collection] Found item without an 'itemKey'`, el);
+    }
+    
+    const { registry } = contextInternal;
+    
+    if (el === null) {
+      console.log('DELETE 1', itemKey);
+      contextInternal.dirty = true;
+      registry.delete(itemKey);
+    } else if (registry.has(itemKey)) {
+      console.error(`[Collection] Found duplicate item key '${itemKey}'`);
+    } else {
+      console.log('SET', itemKey);
+      contextInternal.dirty = true;
+      registry.set(itemKey, el);
+    }
+    
+    return () => {
+      console.log('DELETE 2', itemKey);
+      contextInternal.dirty = true;
+      registry.delete(itemKey);
+    };
+  }, [itemKey, contextInternal]);
+  
+  return {
+    ref,
+    'data-bk-coll-parent': context.componentId,
+    'data-bk-coll-item': itemKey,
+  };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { type StoreApi, createStore, useStore } from 'zustand';
+
+
+/** Definition of a registered item. */
+export type ItemDef = {};
+export type ItemMap = Map<ItemKey, ItemDef>;
+export type ItemWithKey = ItemDef & { itemKey: ItemKey };
+
+export type ItemDetails = { itemKey: ItemKey, label: string };
+
+
+
+//const internalKey = Symbol('baklava.ListBox.internal');
+type ListBoxInternal = {
+  /**
+   * Used to track the items that have been rendered. Note: this property is internal only, it should not be accessed
+   * by consumers. The `Map` is mutated in place for performance, since there may be a lot of items. On initial render,
+   * we get N registrations, an immutable update taking O(N) would result in O(N^2) updates which is not acceptable.
+   */
+  itemsRegistry: ItemMap,
+  
+  /**
+   * Keep track of the items count. Cannot be derived from `itemsRegistry`, because it is mutable and so does
+   * not trigger updates.
+   */
+  itemsCount: number,
+};
+
+export type ListBoxState = {
+  /** Internal bookkeeping. */
+  [internalKey]: ListBoxInternal,
+  
+  /** Globally unique ID for the list box control (e.g. for ARIA attributes). */
+  id: string,
+  
+  /** Whether the list box control is currently disabled or not. */
+  disabled: boolean,
+  
+  /** The currently focused item (if any). */
+  focusedItem: null | ItemKey,
+  
+  /** The currently selected item (if any). */
+  selectedItem: null | ItemKey,
+  
+  /**
+   * If the list is virtually rendered, `virtualItemKeys` should be provided with the full list of item keys. This list
+   * should be ordered in the same way as the items are rendered on screen.
+   */
+  virtualItemKeys: null | VirtualItemKeys,
+};
+
+
+export const createListBoxStore = (options: ListBoxOptions) => {
+  const internal: ListBoxInternal = {
+    itemsRegistry: new Map(),
+    itemsCount: 0,
+  };
+  const propsWithDefaults: ListBoxState = {
+    [internalKey]: internal,
+    disabled: false,
+    focusedItem: null,
+    selectedItem: null,
+    virtualItemKeys: null,
+    ...props,
+  };
+  return createStore<ListBoxStateApi>()((set, get) => ({
+    ...propsWithDefaults,
+    isEmpty: () => get()[internalKey].itemsCount === 0,
+    setVirtualItemKeys: virtualItemKeys => set({ virtualItemKeys }),
+    getItemPosition: (itemKey: ItemKey): null | number => {
+      const state = get();
+      
+      const virtualItemKeys = state.virtualItemKeys;
+      if (virtualItemKeys) {
+        return VirtualItemKeysUtil.indexForItemKey(virtualItemKeys, itemKey);
+      } else {
+        // FIXME 1: cache the reverse mapping (track the index)
+        // FIXME 2: there's no guarantee that the registry is ordered by DOM position
+        return VirtualItemKeysUtil.indexForItemKey([...state[internalKey].itemsRegistry.keys()], itemKey);
+      }
+    },
+    focusItem: itemKey => {
+      const state = get();
+      if (state.focusedItem === itemKey) {
+        // Already focused; avoid redundant focus that might re-enter
+        return;
+      }
+      set({ focusedItem: itemKey });
+      
+      if (itemKey !== null) {
+        const itemTargetRef = get()[internalKey].itemsRegistry.get(itemKey)?.itemRef;
+        
+        // Note: this only works if the item is already rendered, for virtual lists the component will need to handle
+        // the scroll.
+        const element = itemTargetRef?.current as undefined | HTMLElement;
+        if (element && document.activeElement !== element) {
+          element.focus({ focusVisible: false });
+        }
+      }
+    },
+    focusItemAt: position => {
+      const state = get();
+      const virtualItemKeys = state.virtualItemKeys;
+      const registeredItems = state[internalKey].itemsRegistry;
+      const registeredItemKeys = [...registeredItems.keys()]; // FIXME: sort registered items?
+      
+      // Get the complete list of item keys (possibly lazily computed in case of virtualization)
+      const itemKeys: VirtualItemKeys = virtualItemKeys ?? registeredItemKeys;
+      
+      const itemKey = itemKeys.at(position === 'first' ? 0 : -1) ?? null;
+      if (itemKey !== null) {
+        state.focusItem(itemKey);
+      }
+    },
+    selectItem: itemKey => { set({ selectedItem: itemKey }); },
+  }));
+};
+export type ListBoxStore = ReturnType<typeof createListBoxStore>;
