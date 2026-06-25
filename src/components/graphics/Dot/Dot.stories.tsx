e931787b1db1152a547668da28fb07a4ee602396
Arlindo Pereira <397851+nighto@users.noreply.github.com>
2026-06-25 18:59:42 +0200

    Add story with paragraphs

--- src/components/graphics/Dot/Dot.stories.tsx
+++ src/components/graphics/Dot/Dot.stories.tsx
@@ -8,6 +8,8 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import { Dot } from './Dot.tsx';
 
+import { LoremIpsum } from '../../../util/storybook/LoremIpsum.tsx';
+
 
 type DotArgs = React.ComponentProps<typeof Dot>;
 type Story = StoryObj<typeof Dot>;
@@ -21,32 +23,67 @@ export default {
   argTypes: {},
   args: {},
   render: args => <Dot {...args}/>,
-  decorators: [
-    Story => <div style={{ display: 'flex', gap: '8px', 'align-items': 'center' }}><Story/> Dot</div>,
-  ],
 } satisfies Meta<DotArgs>;
 
 
+// it is not possible for a story to completely replace a default decorator, so it is repeated in some stories
+// see https://storybook.js.org/docs/writing-stories/decorators#decorator-inheritance
+// and https://github.com/storybookjs/storybook/issues/12670
 export const DotAlert: Story = {
   args: {
     event: 'alert',
-  }
+  },
+  decorators: [
+    Story => <div style={{ display: 'flex', gap: '8px', 'align-items': 'center' }}><Story/> Dot</div>,
+  ],
 };
 
 export const DotInformational: Story = {
   args: {
     event: 'informational',
-  }
+  },
+  decorators: [
+    Story => <div style={{ display: 'flex', gap: '8px', 'align-items': 'center' }}><Story/> Dot</div>,
+  ],
 };
 
 export const DotSuccess: Story = {
   args: {
     event: 'success',
   },
+  decorators: [
+    Story => <div style={{ display: 'flex', gap: '8px', 'align-items': 'center' }}><Story/> Dot</div>,
+  ],
 };
 
 export const DotWarning: Story = {
   args: {
     event: 'warning',
-  }
+  },
+  decorators: [
+    Story => <div style={{ display: 'flex', gap: '8px', 'align-items': 'center' }}><Story/> Dot</div>,
+  ],
+};
+
+export const DotWithParagraphAndNoExtraStyling: Story = {
+  args: {
+    event: 'success',
+  },
+  decorators: [
+    Story => <><Story/><LoremIpsum paragraphs={1}/></>,
+  ],
+};
+
+export const DotWithParagraphAndAdditionalStyling: Story = {
+  args: {
+    event: 'success',
+  },
+  decorators: [
+    Story => (
+      <div style={{ display: 'grid', gap: '8px', 'align-items': 'center', 'grid-auto-flow': 'column' }}>
+        <Story/>
+        <LoremIpsum/>
+      </div>
+    ),
+  ],
 };
